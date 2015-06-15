8d783e2cbe4530f69d32b389dde756fcbe2f874b
Clicia Scarlet <yuyuvn@mac.com>
2015-06-15 10:33:52 +0700

    task/31860 User can login / logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ActiveRecord::Base
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
   has_many :following, through: :active_relationships,  source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower     
+  has_many :followers, through: :passive_relationships, source: :follower
+  
+  has_secure_password
 end
