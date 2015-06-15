3d6916f473c7c38fd8886c68723eacd2f69e6dc2
Clicia Scarlet <yuyuvn@mac.com>
2015-06-15 14:03:51 +0700

    task/32839 User can edit profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
+  def edit
+    @user = User.find params[:id]
+  end
+  
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes user_params
+      flash[:success] = I18n.t :profile_updated_message
+      redirect_to root_url
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+  
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = I18n.t :request_login_message
+      redirect_to login_url
+    end
+  end
+end
