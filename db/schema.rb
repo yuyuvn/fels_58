47a6f9a2b88d53c8825fe4942f20c30b3687456f
Clicia Scarlet <yuyuvn@mac.com>
2015-06-15 10:33:52 +0700

    task/31860 User can login / logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529021630) do
+ActiveRecord::Schema.define(version: 20150615015938) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "target_id",  limit: 4
@@ -75,11 +75,12 @@ ActiveRecord::Schema.define(version: 20150529021630) do
   add_index "results", ["word_id"], name: "index_results_on_word_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.boolean  "is_admin",   limit: 1,   default: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.boolean  "is_admin",        limit: 1,   default: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.string   "password_digest", limit: 255
   end
 
   create_table "words", force: :cascade do |t|
