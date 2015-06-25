3171984c166fad8e0715c39d1d5f0ad320ae154e
Clicia Scarlet <yuyuvn@mac.com>
2015-06-25 15:16:11 +0700

    Task/32891 User can filter the words list by some conditions

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,11 @@
 class WordsController < ApplicationController
   def index
-    category = Category.find params[:category_id]
-    @words = category.words.page params[:page]
+    @category = Category.find params[:category_id]
+    @filter_state = params[:filter_state] || Settings.category_type.all
+    @words = @category.filter_words(current_user, @filter_state)
+      .page params[:page]
+    @categories_list = Category.all.collect{|category|
+      [category.name, category_words_path(category)]}
     
     respond_to do |format|
       format.html
