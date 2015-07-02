e04fd6b75665534557196eab661857f0c2d10217
Clicia Scarlet <yuyuvn@mac.com>
2015-07-02 09:29:30 +0700

    Task/33970 Check if user already learned all word in lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -9,18 +9,28 @@ class Lesson < ActiveRecord::Base
   
   accepts_nested_attributes_for :results
   
-  after_initialize ->(){self.correct_number ||= 0}
-  before_create ->(){self.words = category.words.not_learned(user).some}
+  after_initialize ->{self.correct_number ||= 0}
+  before_create ->{self.words = category.words.not_learned(user).some}
   before_save :update_correct_number
-  after_save :create_learned_log, if: ->(lesson){lesson.finished?}
+  after_save :create_learned_log, if: ->lesson{lesson.finished?}
   after_destroy :destroy_activities
+  
+  validate :check_words, on: :create
+  
+  scope :unfinished, ->{joins(:results).where results: {answer_id: nil}}
     
   def finished?
-    results.unanswered.count == 0
+    !results.unanswered.any?
   end
   
   private  
   def update_correct_number
     self.correct_number = results.select{|result| result.is_correct?}.count
+  end
+  
+  def check_words
+    unless category.words.not_learned(user).any?
+      self.errors.add :base, I18n.t("lessons.errors.no_word_available")
+    end
   end
 end
